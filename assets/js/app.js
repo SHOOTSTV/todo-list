8786dac61642c45c8d17f71514ea270641f8c160
AxelS <72924802+SHOOTSTV@users.noreply.github.com>
2025-11-18 18:31:18 +0100

    refactor: Remove unused Todo instances from app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,25 +1,9 @@
 import TaskController from "./TaskController.js";
-import Todo from "./Todo.js";
 import TodoList from "./TodoList.js";
 import TodoRender from "./TodoRender.js";
 
 const todoList = new TodoList();
 const todoRender = new TodoRender();
-const todo1 = new Todo(
-  crypto.randomUUID(),
-  "Task 1",
-  "Description 1",
-  "2025-11-16",
-  "High"
-);
-
-const todo2 = new Todo(
-  crypto.randomUUID(),
-  "Task 2",
-  "Dsqdqsdqij",
-  "2025-11-16",
-  "Medium"
-);
 
 const taskController = new TaskController(todoList, todoRender);
 taskController.init();
